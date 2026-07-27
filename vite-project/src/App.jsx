5f8948f7d53b1d61394ccac2d5764934d92e4f1d
Abhishek kumar <abhishekkumastudent1234@gmail.com>
2026-07-28 04:25:51 +0530

    Hello

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [selectedCategory, setCategory] = useState("All");
 
   return (
-    <main className='flex w-screen'>
+    <main className='flex '>
       <div className='flex flex-col w-50 m-3 h-screen  border-gray-100 border-2 p-5  items-center'>
         <ShoppingBag size={24} />
         <Category
