01e1f1341fe664c7135b37d194c0ccc2fcf66cfe
Abhishek kumar <abhishekkumastudent1234@gmail.com>
2026-07-28 04:23:52 +0530

    Hello

--- vite-project/src/Components/ProductGrid.jsx
+++ vite-project/src/Components/ProductGrid.jsx
@@ -15,7 +15,7 @@ function ProductGrid({ selectedCategory  }) {
   return (
     <main>
       <Searchbar/>
-      <div className="grid grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 
       {filteredProducts.map((product) => (
         <ProductCard
